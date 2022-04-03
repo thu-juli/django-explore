b00606363287ed0f980d7323421a53ba3d857a1f
Thu Juli <thujuli037@gmail.com>
2022-04-04 07:33:32 +0800

    modelform

--- explore/project/views.py
+++ explore/project/views.py
@@ -1,36 +1,22 @@
 from django.shortcuts import render
 from .models import Project
-
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from .forms import ProjectForm
 
 
 def projects(request):
     projects = Project.objects.all()
-    content = {
+    context = {
         'projectlist': projects,
     }
-    return render(request, 'project/projects.html', content)
+    return render(request, 'project/projects.html', context)
 
 
 def project(request, pk):
-
     projectObj = Project.objects.get(id=pk)
-
     return render(request, 'project/single-project.html', {'project': projectObj})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'project/project-form.html', context)
