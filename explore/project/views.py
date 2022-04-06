a4be3186ea87ffb8dd81c20b5a613eed7078e5e5
Thu Juli <thujuli037@gmail.com>
2022-04-06 08:08:52 +0800

    create and update database

--- explore/project/views.py
+++ explore/project/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
 
@@ -18,5 +18,29 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
     context = {'form': form}
+    return render(request, 'project/project-form.html', context)
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {
+        'form': form
+    }
+
     return render(request, 'project/project-form.html', context)
