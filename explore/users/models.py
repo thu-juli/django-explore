2757968143b4aaedee043ad5bc1bff03bff374b5
Thu Juli <thujuli037@gmail.com>
2022-04-13 08:06:01 +0800

    slicing profiles page

--- explore/users/models.py
+++ explore/users/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(max_length=254, null=True, blank=True)
     username = models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
     short_intro = models.CharField(max_length=255, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     profile_image = models.ImageField(
@@ -24,3 +25,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,
+                          primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return self.name
