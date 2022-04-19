74c73ae0893d15efc80bc6a0f4b1190d027b3cbd
Thu Juli <thujuli037@gmail.com>
2022-04-19 23:56:04 +0800

    login and logout user from web request

--- explore/users/views.py
+++ explore/users/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
+def loginUser(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+
+        if request.user.is_authenticated:
+            return redirect('profiles')
+
+    return render(request, 'users/login-register.html')
+
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, 'User was loged out!')
+    return redirect('login')
 
 
 def profiles(request):
